95406f2b4f23462c3f5fbb3d2dfbe3a4c03711a2
binh.buidang <dangbinh4869@gmail.com>
2024-01-14 13:20:58 +0700

    Update Button component styles

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -11,9 +11,9 @@ const Button = forwardRef<HTMLButtonElement, ButtonProps>(
         type={type}
         className={`
           rounded-md border border-input
-          bg-slate-500 bg-transparent px-2 py-1
+          bg-zinc-500 px-2 py-1
+          hover:bg-zinc-600
           text-white text-sm shadow-sm
-          hover:bg-slate-600
           transition-colors file:border-0
           file:bg-transparent file:text-sm
           file:font-medium placeholder:text-muted-foreground
