f61e251a2b8ca9b80bea8c524d04b36d67afbdfb
binh.buidang <dangbinh4869@gmail.com>
2024-01-23 22:22:33 +0700

    update layout

--- frontend/src/pages/JwtDecoder.tsx
+++ frontend/src/pages/JwtDecoder.tsx
@@ -80,49 +80,57 @@ const JWTDecoder: React.FC = () => {
       </div>
       <div className="w-1/2 pl-4">
         <label className="block mb-2 font-semibold">Decoded Token</label>
-        <div className="flex flex-col justify-center gap-2.5 border border-gray-300 rounded-md px-4 h-5/6">
-          <span className="text-sm">Header (algorithm and token type)</span>
-          <ReactJSON
-            theme="tube"
-            src={decodedToken?.header || {}}
-            quotesOnKeys={false}
-            name={null}
-            style={jsonViewerStyles}
-          />
-          <span className="text-sm mt-2">Payload</span>
-          <ReactJSON
-            theme="google"
-            src={decodedToken?.payload || {}}
-            quotesOnKeys={false}
-            name={null}
-            style={jsonViewerStyles}
-          />
-          <span className="text-sm mt-2">Signature</span>
-          <pre className="border p-4 rounded-md bg-gray-800 flex flex-col gap-1 text-blue-300">
-            <span className="font-semibold">HMACSHA256(</span>
-            <span className="ml-5">base64UrlEncode(header) + "." +</span>
-            <span className="ml-5">base64UrlEncode(payload),</span>
-            <input
-              type="text"
-              name="secret"
-              value="your-256-bit-secret"
-              className="border rounded-md px-2 py-1 focus:outline-none focus:ring focus:border-blue-300 ml-5"
-              data-tippy=""
-              data-original-title="Weak secret!"
-            />
-            <span className="is-base64-encoded-label">
-              ){' '}
-              <input
-                id="is-base64-encoded"
-                type="checkbox"
-                name="is-base64-encoded"
-                className="mr-2"
+        <div className="h-5/6 border border-gray-300 rounded-md">
+          <div className="flex flex-col justify-evenly gap-2.5 px-4 pt-4">
+            <div className="flex flex-col space-y-2">
+              <span className="text-sm">Header (algorithm and token type)</span>
+              <ReactJSON
+                theme="tube"
+                src={decodedToken?.header || {}}
+                quotesOnKeys={false}
+                name={null}
+                style={jsonViewerStyles}
               />
-              <label htmlFor="is-base64-encoded" className="cursor-pointer">
-                secret base64 encoded
-              </label>
-            </span>
-          </pre>
+            </div>
+            <div className="flex flex-col space-y-2">
+              <span className="text-sm mt-2">Payload</span>
+              <ReactJSON
+                theme="google"
+                src={decodedToken?.payload || {}}
+                quotesOnKeys={false}
+                name={null}
+                style={jsonViewerStyles}
+              />
+            </div>
+            <div className="flex flex-col space-y-2">
+              <span className="text-sm mt-2">Signature</span>
+              <pre className="border p-4 rounded-md bg-gray-800 flex flex-col gap-1 text-blue-300">
+                <span className="font-semibold">HMACSHA256(</span>
+                <span className="ml-5">base64UrlEncode(header) + "." +</span>
+                <span className="ml-5">base64UrlEncode(payload),</span>
+                <input
+                  type="text"
+                  name="secret"
+                  value="your-256-bit-secret"
+                  className="border rounded-md px-2 py-1 focus:outline-none focus:ring focus:border-blue-300 ml-5"
+                  data-tippy=""
+                  data-original-title="Weak secret!"
+                />
+                <span className="is-base64-encoded-label">
+                  ){' '}
+                  <input
+                    id="is-base64-encoded"
+                    type="checkbox"
+                    name="is-base64-encoded"
+                    className="mr-2"
+                  />
+                  <label htmlFor="is-base64-encoded" className="cursor-pointer">
+                    secret base64 encoded
+                  </label>
+                </span>
+              </pre>
+            </div>
+          </div>
         </div>
       </div>
     </div>
